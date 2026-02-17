9b4c2e71864532d7601686eec0bacf483f3e75a9
abdelrhman mohsen <abdo.mohsen3457643@gmail.com>
2026-02-17 20:29:15 +0200

    cors

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -3,7 +3,12 @@ import { ConfigService } from '@nestjs/config';
 export const getCorsConfig = (configService: ConfigService) => {
   const allowedOrigins = configService
     .get<string>('CORS_ORIGINS')
-    ?.split(',') || ['http://localhost:3001', 'https://www.sarhan.shop'];
+    ?.split(',') || [
+    'http://localhost:3001',
+    'https://www.sarhan.shop',
+    'https://sarhan.shop',
+    'https://www.sarhan.shop',
+  ];
 
   return {
     origin: allowedOrigins,
